ef0cf610b6f06ad63b9c49aa48c31f6f78241bc5
ashishpatidar1 <144095594+ashishpatidar1@users.noreply.github.com>
2023-09-05 12:25:24 +0530

    Create WRAPPER TO OBJECT.java

---
+++ WRAPPER TO OBJECT.java
@@ -0,0 +1,22 @@
+public class wrapperExample1{
+    public static void main(String args[]){
+      int i=50;
+      boolean b=true||false;
+      char c='a';
+      byte e=1;
+      short s=8;
+      long l=100;
+      float f =3.14f;
+      double d= 200;
+        System.out.println(i+"\n"+b+"\n"+c+"\n"+e+"\n"+s+"\n"+l+"\n"+f+"\n"+d);
+      int    I = i;
+      boolean B = b;
+      char C =c;
+      byte E=e;
+      short S=s;
+      long L =l;
+      float F=f;
+      double D= d;
+      System.out.println(I+"\n"+B+"\n"+C+"\n"+E+"\n"+S+"\n"+L+"\n"+F+"\n"+D);
+    }
+}
