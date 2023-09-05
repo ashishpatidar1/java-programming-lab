9087523a4d23732d0548efc10ebf9c04b9e48728
ashishpatidar1 <144095594+ashishpatidar1@users.noreply.github.com>
2023-09-05 12:07:02 +0530

    autoboxing2.java

---
+++ autoboxing2.java
@@ -0,0 +1,9 @@
+public class wrapperExample1{
+    public static void main(String args[]){
+        Integer a=new Integer(3);
+        int i=a.intValue();
+        Integer j=a;
+        System.out.println(a+"\n"+i+"\n"+j);
+    }
+}
+// declare the primitive data type with some intial value. conver this primitive data type to using wapper class to object.
